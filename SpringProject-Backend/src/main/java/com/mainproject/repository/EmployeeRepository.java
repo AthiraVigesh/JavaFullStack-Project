dad893a018c52f28414cb3da807ca7a576a09394
AthiraVigesh <124951254+AthiraVigesh@users.noreply.github.com>
2023-04-14 13:51:15 +0530

    Add files via upload

---
+++ SpringProject-Backend/src/main/java/com/mainproject/repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.mainproject.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mainproject.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long>
+{
+
+	List<Employee> findAll();
+
+}
